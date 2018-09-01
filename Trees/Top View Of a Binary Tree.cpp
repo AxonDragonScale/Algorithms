ba2f4196cabb882326a0d9ececabf5f387863e72
Ronak Harkhani <r.r.n.harkhani@gmail.com>
2018-09-01 16:10:20 +0530

    Top View of Binary Tree solved.

---
+++ Trees/Top View Of a Binary Tree.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <string>
+#include <algorithm>
+#include <climits>
+#include <stack>
+
+#define LN cout<<__LINE__<<endl
+
+using namespace std;
+
+struct Node {
+	int data;
+	Node *left, *right;
+
+	Node(int data) {
+		this->data = data;
+		this->left = NULL;
+		this->right = NULL;
+	}
+};
+
+void topView(struct Node *root) {
+    if(root == NULL) {
+        return;
+    }
+    
+    map<int, int> m;
+    queue< pair<Node*, int> > q;
+    q.push({root, 0});
+    
+    Node *cur; int d;
+    while(!q.empty()) {
+        cur = q.front().first;
+        d = q.front().second;
+        q.pop();
+        
+        if(m.find(d) == m.end()) {
+            m[d] = cur->data;
+        }
+        
+        if(cur->left != NULL) {
+            q.push({cur->left, d-1});
+        }
+        if(cur->right != NULL) {
+            q.push({cur->right, d+1});
+        }
+    }
+    
+
+    for(pair<int, int> i : m) {
+        cout<<i.second<<" ";
+    }
+    cout<<endl;
+}
