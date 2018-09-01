e74c6cf2034f0401bc236f3d84cc96dedb598ac4
Ronak Harkhani <r.r.n.harkhani@gmail.com>
2018-09-01 17:23:41 +0530

    Connect Nodes at same level solved.

---
+++ Trees/Connect Nodes at the same Level.cpp
@@ -0,0 +1,38 @@
+
+// https://practice.geeksforgeeks.org/problems/connect-nodes-at-same-level/1/?ref=self
+// https://www.geeksforgeeks.org/connect-nodes-at-same-level/
+// https://www.geeksforgeeks.org/connect-nodes-level-level-order-traversal/
+// https://www.geeksforgeeks.org/connect-nodes-at-same-level-with-o1-extra-space/
+
+struct Node {
+  int data;
+  Node *left,  *right;
+  Node *nextRight;  // This has garbage value in input trees
+};
+
+
+
+void connectNodesAtSameLevel(Node *root) {
+   queue<Node*> q;
+   q.push(root);
+   q.push(NULL);
+   
+   Node *cur;
+   while(!q.empty()) {
+       cur = q.front();
+       q.pop();
+       
+       if(cur == NULL) {
+           if(!q.empty())
+                q.push(NULL);
+           continue;
+       }
+       
+       if(!q.empty()) {
+           cur->nextRight = q.front();
+       }
+       
+       if(cur->left != NULL) q.push(cur->left);
+       if(cur->right != NULL) q.push(cur->right);
+   }
+}
