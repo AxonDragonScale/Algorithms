0a25d63030ae01efb4fa02b79333a587a88fde4d
Ronak Harkhani <r.r.n.harkhani@gmail.com>
2018-09-05 11:44:41 +0530

    Alternative Sol. for First Non-Reapeating Char in a Stream.

--- Questions/First Non-Repeating Character in a Stream.cpp
+++ Questions/First Non-Repeating Character in a Stream.cpp
@@ -29,6 +29,7 @@ int main() {
 	        cin>>str[i];
 	        freq[str[i]-'a'] += 1;
 	        
+		// Iterate through str to find 1st char with freq == 1
 	        flag = 0;
 	        for(int j = 0; j<=i; j++) {
 	            if(freq[str[j]-'a'] == 1) {
@@ -38,6 +39,61 @@ int main() {
 	            }
 	        }
 	        if(flag == 0) cout<<-1<<" ";
+	    }
+	    cout<<endl;
+	}
+	
+	return 0;
+}
+
+// Alternative - Instead of traversing str, we traverse freq (which has only 26 length)
+// and find the char with freq == 1 and minimum index
+
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <string>
+#include <algorithm>
+#include <climits>
+
+#define LN cout<<__LINE__<<endl
+#define iPair pair<int, int>
+
+using namespace std;
+
+int t, n, flag;
+char str[500];
+iPair freq[26]; 
+// first stores freq and second stores index where it last appeared
+
+int main() {
+	
+	cin>>t;
+	while(t--) {
+	    cin>>n;
+	    
+	    for(int i = 0; i<26; i++) {
+	        freq[i].first = 0;
+	        freq[i].second = INT_MAX;
+	    }
+	    
+	    for(int i = 0; i<n; i++) {
+	        cin>>str[i];
+	        freq[str[i]-'a'].first += 1;
+	        freq[str[i]-'a'].second = i; 
+	        
+	        int minIdx = INT_MAX;
+	        for(int i = 0; i<26; i++) {
+	            if(freq[i].first == 1 && freq[i].second < minIdx) {
+	                minIdx = freq[i].second;
+	            }
+	        }
+	        
+	        if(minIdx == INT_MAX) {
+	            cout<<-1<<" ";
+	        } else {
+	            cout<<str[minIdx]<<" ";
+	        }
 	    }
 	    cout<<endl;
 	}
