c1b638b79ac543f2123b2c8612f8a0ffc889dfce
Ronak Harkhani <r.r.n.harkhani@gmail.com>
2018-09-10 11:52:18 +0530

    Create LCM and GCD.cpp

---
+++ Mathematics/LCM and GCD.cpp
@@ -0,0 +1,32 @@
+
+// C++17 has <numeric> which defines gcd(int a, int b) and lcm(int a, int b).
+
+// gcd is basically the product of the intersection of all prime factors of both.
+// C++ also has inbuilt __gcd(int a, int b) function for gcd.
+// euclidean algo
+int gcd(int a, int b) {
+    if(b == 0) {
+        return a;
+    }    
+    return gcd(b, a%b);
+}
+
+// its basically the product of the union of all prime factors of both.
+// C++17 has lcm(int a, int b) inbuilt
+
+// this method is not applicable for more than 2 numbers.
+int lcm(int a, int b) {
+    return a*b/gcd(a, b);
+}
+
+// LCM of an array
+void arrayLCM(vector<int> &arr) {
+	long long int ans;
+	ans = arr[0];
+	
+	for(int i : arr) {
+		ans = ans*i/__gcd(ans, i);
+	}
+	
+	cout<<ans<<endl;
+}
